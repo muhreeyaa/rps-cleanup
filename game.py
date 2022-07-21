7b1e7bc41c340420d2c75cce8126ee6925a61405
muhreeyaa <108751570+muhreeyaa@users.noreply.github.com>
2022-07-21 19:16:18 -0400

    Removed duplication (#1)

--- game.py
+++ game.py
@@ -4,13 +4,14 @@
 
 from random import choice
 
+valid_choices = ["rock", "paper", "scissors"]
 #
 # USER SELECTION
 #
 
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in valid_choices:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -18,7 +19,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(valid_choices)
 print("COMPUTER CHOICE:", c)
 
 #
